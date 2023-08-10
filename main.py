2287216e62fb126179fbd34d261cf6ba00ced2bd
sw-sys <sheldonkorpet@gmail.com>
2023-08-10 12:02:38 +0100

    layout update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import PySimpleGUI as sg
 
-# theme - colours for GUI
-sg.theme('DarkGray5') # 5 best so far, 2, 6 is ok
+# Design
+sg.theme('DarkGray6')
+sg.set_options(font=('Arial 12'), text_color='white')
 
 ### WINDOW CONTENT BEGINS
 
@@ -12,13 +13,15 @@ sg.theme('DarkGray5') # 5 best so far, 2, 6 is ok
 # options for status
 status_options = ['Seen', 'Not seen', 'Unknown']
 
-tab1_layout = [#[[sg.Text('Record your daily dose of Donald', font=('Arial',20))],
-                [sg.CalendarButton('Select Date:', target='date_input', format='%d-%m-%Y'), sg.InputText(key='DATE')],
+tab1_layout = [
+                [sg.Text(' ')],
+                [sg.Text('Status:'), sg.Push(), sg.Combo(status_options, default_value=status_options[0], key='STATUS'), sg.Push(), sg.Push(), sg.Push()],
+                [sg.Text('Enter Date: DD-MM-YYYY'),sg.Input(key='DATE'), sg.Push(), sg.CalendarButton('Select', target='DATE', format='%d-%m-%Y')],
                 [sg.Text('Enter a time (HH:MM:SS):'), sg.Input(key='TIME', enable_events=True)],
-                [sg.Text('Status (seen, not seen, unknown):'), sg.Combo(status_options, default_value=status_options[0], key='STATUS')],
-                [sg.Text('Location (co-ords):'), sg.Input(key='LOCATION')],
-                [sg.Text('Image:'), sg.Input(key='MEDIA'), sg.FileBrowse()],
-                [sg.OK('Submit'), sg.Cancel()]]
+                [sg.Text('Location (53.3924659, -2.0594862):'), sg.Push(), sg.Input(key='LOCATION'), sg.Push()],
+                [sg.Text('Image:'), sg.Push(), sg.Input(key='MEDIA'), sg.FileBrowse()],
+                [sg.Text(' ')],
+                [sg.OK('Submit'), sg.Cancel('Clear'), sg.Push(), sg.Button('Exit')]]
 
 # TAB 2 content
 tab2_layout = [[sg.Text('Overview of when Donald was spotted this month')]]
@@ -29,19 +32,23 @@ tab3_layout = [[sg.Text('Map of Donald\'s locations')]]
 # TAB 4 content
 tab4_layout = [[sg.Text('Photos of Donald')]]
 
+# TAB 5 content
+tab5_layout = [[sg.Text('All Records here and feature to export to .csv')]]
+
 # define layouts for tabs
-tab_group_layout = [[sg.Tab('Submit', tab1_layout), sg.Tab('Calendar', tab2_layout), sg.Tab('Map', tab3_layout), sg.Tab('Media', tab4_layout)]]
+tab_group_layout = [[sg.Tab('Submit', tab1_layout), sg.Tab('Calendar', tab2_layout), sg.Tab('Map', tab3_layout), sg.Tab('Media', tab4_layout), sg.Tab('Records', tab5_layout)]]
 tab_group = sg.TabGroup(tab_group_layout)
 
-layout = [[[sg.Text('Record and View Donald Sightings', font=('Arial',20))],
+layout = [[[sg.Text('Where\'s Donald Today?', font=('Arial',20))], 
+           [sg.Text(' ')],
            tab_group]]
 
 # window event
-window = sg.Window('Donald\'s Diary', layout)
+window = sg.Window('Donald\'s Diary', layout, no_titlebar=True, grab_anywhere=True)
 
 while True:
     event, values=window.read()
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED or 'Exit':
         break
     elif event == sg.OK():
         time = values['TIME']
--- read_me.txt
+++ read_me.txt
@@ -2,23 +2,3 @@ Notes:
 
 Activate env - conda activate donald
 Docs - https://www.pysimplegui.org/en/latest/#pysimplegui-users-manual
-
-Broken: tab_group_layout / tab1_layout 
-
-- define the layout for each tab before you reference it in the 'tab' element 
-OR 
-- typo
-OR
-- tab1_layout variable not defined before referenced...or is out of scope
-
-in relation to...
-### WINDOW CONTENT BEGINS
-
-# define layouts for tabs
-# tabs for views
-tab_group_layout = [[sg.Tab('Submit', tab1_layout), sg.Tab('Calender', tab2_layout), sg.Tab('Map', tab3_layout), sg.Tab('Media', tab4_layout)]]
-
-# TAB 1 - data submission
-# options for status
-options = ['Seen', 'Not seen', 'Unknown'],
-tab1_layout =
