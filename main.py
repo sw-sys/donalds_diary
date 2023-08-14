d07a9a6d70e0a07463d342f74ced9b74f6791c5d
sw-sys <sheldonkorpet@gmail.com>
2023-08-14 10:09:18 +0100

    add export to csv func

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import PySimpleGUI as sg
-import sqlite3
 import csv
+import sqlite3
+import PySimpleGUI as sg
 
 ### DATABASE
 
 conn = sqlite3.connect('donalds_data.db')
 c = conn.cursor() # cursor allows interigation of data
-c.execute("""CREATE TABLE IF NOT EXISTS sightings (
-          status text,
+c.execute("""CREATE TABLE IF NOT EXISTS sightings
+          (status text,
           date text,
           time text,
-          location real
-          )""")
+          location real)
+          """)
 
 conn.commit()
 conn.close()
@@ -37,7 +37,7 @@ layout = [
     [sg.Text('Enter a time HH:MM:SS'), sg.Push(), sg.Input(key='TIME', enable_events=False), sg.Push(), sg.Push(), sg.Push(), sg.Push()],
     [sg.Text('Location (co-ordinates)'), sg.Push(), sg.Input(key='LOCATION'), sg.Push(), sg.Push(), sg.Push(), sg.Push()],
     [sg.Text(' ')],
-    [sg.Button('Submit'), sg.Button('Clear'), sg.Push(), sg.Button('Show records'), sg.Button('Save to CSV'), sg.Push(), sg.Button('Exit')]
+    [sg.Button('Submit'), sg.Button('Clear'), sg.Push(), sg.Button('Show records'), sg.Button('Save to CSV', key='save_button'), sg.Push(), sg.Button('Exit')]
 ]
 
 # window event
@@ -92,7 +92,6 @@ def clear_inputs():
         window['TIME'].update('')
         window['DATE'].update('')
         window['STATUS'].update('')
-        #window['MEDIA'].update('')
         window['LOCATION'].update('')
     return None
 
@@ -110,28 +109,25 @@ def save_data_to_database():
     conn.commit()
     conn.close()
 
-# def export_to_csv():
-#     conn = sqlite3.connect('donalds_date.db')
-#     c = conn.cursor()
-#     query = "SELECT * FROM sightings"
-#     c.execute(query)
-#     with open('sightings.csv', 'w', newline='') as csvfile:
-#         writer = csv.writer(csvfile)
-#         writer.writerow(['Status', 'Date', 'Time', 'Location'])
-#         for row in c:
-#             writer.writerow(row)
-#     conn.close()
+def save_to_csv():
+    with open('sightings_records.csv', 'w', encoding='UTF8', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
 
 while True:
     event, values=window.read()
     if event in (sg.WIN_CLOSED or 'Exit'):
+        conn.close()
         break
     if event == 'Clear':
         clear_inputs()
     if event == 'Show records':
         create_records()
-    # if event == 'Save to CSV':
-    #     export_to_csv()
+    if event == 'save_button':
+        save_data_to_database()
+        data = get_sightings()
+        save_to_csv()
+        sg.PopupQuick('Data saved to CSV file')
     if event == 'Submit':
         status = values['STATUS']
         if status == '':
@@ -147,9 +143,9 @@ while True:
             sg.PopupError('QUACK ALERT! Please insert a location')
         else:
             try:
-                summary_list = "The following bread crumbs have been added to the quackbase"
-                choice = sg.PopupOKCancel(summary_list, 'Please confirm entry')
-                if choice =='OK':
+                SUMMARY_LIST = "The following bread crumbs have been added to the quackbase"
+                CHOICE = sg.PopupOKCancel(SUMMARY_LIST, 'Please confirm entry')
+                if CHOICE =='OK':
                     clear_inputs()
                     save_data_to_database()
                     sg.PopupQuick('Saved to quackbase and exported to csv!')
